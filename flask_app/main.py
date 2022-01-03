9691664e812fd5a041bfbd6ce784ff3ecf123e9d
johanijbabaj <khaidukov.eugeniy@gmail.com>
2022-01-04 00:37:56 +0300

    add flask service

---
+++ flask_app/main.py
@@ -0,0 +1,15 @@
+"""Запуск основного приложения сервиса аутентификации"""
+# flask_app/app.py
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+    @app.route('/hello-world')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
