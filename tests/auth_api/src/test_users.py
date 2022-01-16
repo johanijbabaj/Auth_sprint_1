ec7453480af412bf2859e2e690d0c8c598dee888
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-01-16 17:12:13 +0300

    Сделал пагинацию для истории действий пользователя, изменены снова файлы db_models.py и app.py, добавил пагинацию для списка пользователей

--- tests/auth_api/src/test_users.py
+++ tests/auth_api/src/test_users.py
@@ -54,3 +54,21 @@ def test_nobody_history():
     # который был только что
     assert isinstance(data, list)
     assert len(data) >= 1
+
+
+def test_nobody_history_paginated():
+    """Просмотр пользователем без прав истории своих действий постранично"""
+    ans = requests.post(f"http://{AUTH_API_HOST}/v1/user/login?login=nobody&password={os.getenv('NOBODY_PASSWORD')}")
+    assert ans.status_code == 200
+    data = ans.json()
+    token = data['access_token']
+    ans = requests.get(
+        f"http://{AUTH_API_HOST}/v1/user/history?page_size=3&page_number=1",
+        headers={'Authorization': 'Bearer ' + token}
+    )
+    assert ans.status_code == 200
+    data = ans.json()
+    # В истории должна быть хотя бы одна запись - от того входа,
+    # который был только что
+    assert isinstance(data, list)
+    assert len(data) >= 1
